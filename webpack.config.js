95c70bc87115c655a4121fdac98ac9a620684587
Brett Snaidero <brettsnaidero@hotmail.com>
2016-05-27 08:33:37 +1000

    Changing stuff to production state

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     entry: getEntrySources(['./src/js/entry.js']),
     output: {
-        publicPath: 'http://localhost:8080/',
+        publicPath: '',
         filename: 'build/bundle.js'
     },
     devtool: 'eval',
@@ -48,10 +48,10 @@ module.exports = {
 };
 
 function getEntrySources(sources) {
-    // if (process.env.NODE_ENV !== 'production') {
+    if (process.env.NODE_ENV !== 'production') {
         sources.push('webpack-dev-server/client?http://localhost:8080');
         sources.push('webpack/hot/only-dev-server');
-    // }
+    }
 
     return sources;
 }
